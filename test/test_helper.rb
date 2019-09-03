639596062a21f6f190b3c176b634c81d24c295e4
Michaela Morrow <morrow.michaela@gmail.com>
2019-09-03 14:57:06 -0700

    Added SimpleCov and added require_relative for my lib files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,14 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require 'simplecov'
+SimpleCov.start
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative "../lib/block.rb"
+require_relative "../lib/date_range.rb"
+require_relative "../lib/room.rb"
+require_relative "../lib/reservation.rb"
+require_relative "../lib/manager.rb"
