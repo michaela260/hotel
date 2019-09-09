d9d61428d2437c82713b703acb38b383107c618e
Michaela Morrow <morrow.michaela@gmail.com>
2019-09-09 00:13:52 -0700

    Updated 'make_reservation' method to assign only available rooms; added 'find_room', 'find_reservation', and 'list_of_available_rooms_by_date' methods

--- lib/manager.rb
+++ lib/manager.rb
@@ -2,18 +2,6 @@ require_relative 'room'
 require_relative 'date_range'
 require_relative 'reservation'
 
-# create Hotel module
-#create Manager class
-
-# Initialize Manager with list_of_all_rooms
-
-# method make_reservation (creates instance of reservation)
-# make sure that it produces a room number and reservation number that are not repeats
-
-# method list_of_reservations_by_date
-
-# method list_of_available_rooms_by_date
-
 # method reserve_block (creates instance of block)
 
 module Hotel
@@ -35,7 +23,16 @@ module Hotel
     def make_reservation(start_date: , end_date: )
       
       new_date_range = Hotel::DateRange.new(start_date: start_date, end_date: end_date)
-      new_room_number = @list_of_all_rooms.sample.room_number
+      possible_room_numbers = (1..20).to_a
+      @list_of_all_reservations.each do |reservation|
+        if reservation.date_range.overlap?(new_date_range)
+          possible_room_numbers.delete(reservation.room_number)
+        end
+      end
+      if possible_room_numbers.empty?
+        raise ArgumentError.new "Error! All rooms are reserved during the date range you requested. Sorry :("
+      end
+      new_room_number = possible_room_numbers.first
       new_reservation_number = @list_of_all_reservations.length + 100
       
       new_reservation = Hotel::Reservation.new(date_range: new_date_range, room_number: new_room_number, reservation_number: new_reservation_number)
@@ -53,6 +50,50 @@ module Hotel
       end
       
       return list_of_reservations_by_date
+    end
+    
+    def find_room(room_number_to_search)
+      if room_number_to_search < 1 || room_number_to_search > 20 || room_number_to_search.class != Integer
+        raise ArgumentError.new "Error! You entered an invalid room number: #{room_number_to_search}"
+      end
+      
+      return @list_of_all_rooms.find { |room| room.room_number == room_number_to_search }
+    end
+    
+    def find_reservation(reservation_number_to_search)
+      indicator = 0
+      @list_of_all_reservations.each do |reservation|
+        if reservation.reservation_number != reservation_number_to_search
+          indicator += 1
+        end
+      end
+      
+      if indicator == @list_of_all_reservations.length
+        raise ArgumentError.new "Error! You entered an invalid reservation number: #{reservation_number_to_search}"
+      end
+      
+      return @list_of_all_reservations.find { |reservation| reservation.reservation_number == reservation_number_to_search}
+      
+    end
+    
+    def list_of_available_rooms_by_date(date_to_check: )
+      unavailable_room_numbers = list_of_reservations_by_date(date_to_search: date_to_check).map do |reservation|
+        reservation.room_number
+      end
+      
+      available_room_numbers = @list_of_all_rooms.map do |room|
+        room.room_number
+      end
+      
+      unavailable_room_numbers.each do |room_number|
+        available_room_numbers.delete(room_number)
+      end
+      
+      available_rooms = available_room_numbers.map do |room_number|
+        find_room(room_number)
+      end
+      
+      return available_rooms
     end
     
   end
