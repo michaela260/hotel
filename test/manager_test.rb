0606291f383e5a64d7b9b0c31a8b4c990ba46bed
Michaela Morrow <morrow.michaela@gmail.com>
2019-09-09 00:16:06 -0700

    Updated unit tests to check that reservations are only made for available rooms; also added tests for 'list_of_available_rooms_by_date', 'find_room', and 'find_reservation' methods

--- test/manager_test.rb
+++ test/manager_test.rb
@@ -5,6 +5,8 @@ describe "Manager class" do
   before do
     @test_hotel = Hotel::Manager.new
     @test_hotel_2 = Hotel::Manager.new
+    @test_hotel_3 = Hotel::Manager.new
+    @test_hotel_4 = Hotel::Manager.new
     
     @test_date_1 = Date.today
     @test_date_2 = Date.today.prev_day
@@ -70,6 +72,21 @@ describe "Manager class" do
       expect(@test_reservation_1.room_number).must_be :>=, 1
     end
     
+    it "chooses the first room number that is available during the given date range" do
+      expect(@test_reservation_1.room_number).must_equal 1
+      expect(@test_reservation_2.room_number).must_equal 2
+      expect(@test_reservation_3.room_number).must_equal 3
+      expect(@test_reservation_4.room_number).must_equal 1
+      expect(@test_reservation_5.room_number).must_equal 2
+    end
+    
+    it "raises an ArgumentError if no rooms are available during the given date range" do
+      20.times do |i|
+        @test_hotel_3.make_reservation(start_date: @test_date_1, end_date: @test_date_5)
+      end
+      expect{@test_hotel_3.make_reservation(start_date: @test_date_1, end_date: @test_date_5)}.must_raise ArgumentError
+    end
+    
     it "generates a reservation numbers in order, starting at 100" do
       expect(@test_reservation_1.reservation_number).must_be_instance_of Integer
       expect(@test_reservation_1.reservation_number).must_equal 100
@@ -97,6 +114,7 @@ describe "Manager class" do
       expect(@test_hotel.list_of_reservations_by_date(date_to_search: Date.today)).wont_include @test_reservation_1
       expect(@test_hotel.list_of_reservations_by_date(date_to_search: Date.today)).wont_include @test_reservation_2
       expect(@test_hotel.list_of_reservations_by_date(date_to_search: Date.today)).wont_include @test_reservation_5
+      expect(@test_hotel.list_of_reservations_by_date(date_to_search: Date.today).length).must_equal 2
     end
     
     it "includes all reservations for the given date" do
@@ -106,27 +124,76 @@ describe "Manager class" do
     
   end
   
-  # describe "list_of_available_rooms_by_date method" do
+  describe "list_of_available_rooms_by_date method" do
+    
+    it "returns an array of rooms" do
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today)).must_be_instance_of Array
+      @test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today).each do |room|
+        expect(room).must_be_instance_of Hotel::Room
+      end
+    end
+    
+    it "only includes rooms that are available on the given date" do
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today)).wont_include @test_hotel.find_room(@test_reservation_3.room_number)
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today)).wont_include @test_hotel.find_room(@test_reservation_4.room_number)
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today).length).must_equal 18
+    end
+    
+    it "includes all rooms that are available on the given date" do
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today)).must_include @test_hotel.find_room(@test_reservation_2.room_number)
+      expect(@test_hotel.list_of_available_rooms_by_date(date_to_check: Date.today)).must_include @test_hotel.find_room(@test_reservation_5.room_number)
+    end
+    
+    it "includes rooms that have a previous reservation ending on the given date" do
+      ended_reservation = @test_hotel_4.make_reservation(start_date: Date.today.prev_day, end_date: Date.today)
+      expect(@test_hotel_4.list_of_available_rooms_by_date(date_to_check: Date.today)).must_include @test_hotel_4.find_room(ended_reservation.room_number)
+    end
+    
+  end
   
-  #   it "returns an array of rooms" do
-  #     expect(test_hotel.list_of_available_rooms_by_date(Date.today)).must_be_instance_of Array
-  #     test_hotel.list_of_available_rooms_by_date(Date.today).each do |room|
-  #       expect(room).must_be_instance_of Hotel::Room
-  #     end
-  #   end
+  describe "find_room method" do
+    before do
+      @found_room = @test_hotel.find_room(@test_reservation_4.room_number)
+    end
+    
+    it "returns an instance of Hotel::Room" do
+      expect(@found_room).must_be_instance_of Hotel::Room
+    end
+    
+    it "returns a room whose room_number matches the room number provided" do
+      expect(@found_room.room_number).must_equal 1
+      expect(@found_room).must_equal @test_hotel.list_of_all_rooms[0]
+    end
+    
+    it "raises an ArgumentError if an invalid room number is provided" do 
+      expect{@test_hotel.find_room(0)}.must_raise ArgumentError
+      expect{@test_hotel.find_room("9banana")}.must_raise ArgumentError
+      expect{@test_hotel.find_room(-1)}.must_raise ArgumentError
+      expect{@test_hotel.find_room(1.5)}.must_raise ArgumentError
+    end
+  end
   
-  #   it "only includes rooms that are available on the given date" do
-  
-  #   end
-  
-  #   it "includes all rooms that are available on the given date" do
-  
-  #   end
-  
-  #   it "includes rooms that have a previous reservation ending on the given date" do
-  
-  #   end
-  
-  #   end
+  describe "find_reservation method" do
+    before do
+      @found_reservation = @test_hotel.find_reservation(@test_reservation_1.reservation_number)
+    end
+    
+    it "returns an instance of Hotel::Reservation" do
+      expect(@found_reservation).must_be_instance_of Hotel::Reservation
+    end
+    
+    it "returns a reservation whose reservation_number matches the reservation number provided" do
+      expect(@found_reservation.reservation_number).must_equal 100
+      expect(@found_reservation).must_equal @test_hotel.list_of_all_reservations[0]
+    end
+    
+    it "raises an ArgumentError if an invalid reservation number is provided" do
+      expect{@test_hotel.find_reservation(99)}.must_raise ArgumentError
+      expect{@test_hotel.find_reservation(0)}.must_raise ArgumentError
+      expect{@test_hotel.find_reservation(-100)}.must_raise ArgumentError
+      expect{@test_hotel.find_reservation(100.5)}.must_raise ArgumentError
+      expect{@test_hotel.find_reservation("9banana")}.must_raise ArgumentError
+    end
+  end
   
 end
